f75641bc973fc33b639eebdba07778e1e7c5bc00
xhc <1031568754@qq.com>
2018-12-19 00:01:36 +0800

    完善实时热点页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,10 @@ import request from '../../utils/request'
 Page({
   data: {
     status: 'loading',
-    hotList: []
+    hotList: [],
+    // 搜索相关
+    searchWord: '',
+    showModal: false,  // 输入框得焦，背景虚化，
   },
   onLoad: function () {
     this.getLatestData()
@@ -23,6 +26,11 @@ Page({
       this.setData({
         status: resList.length ? 'rendering' : 'error',
         hotList: resList
+      }, function() {
+        // wx.pageScrollTo({
+        //   scrollTop: 40,
+        //   duration: 300
+        // })
       })
     }).catch(err => {
       console.log(err)
@@ -30,6 +38,28 @@ Page({
         status: 'error'
       })
     })
+  },
+  /* 搜索相关 */
+  // 得焦
+  bindFocus(e) {
+    console.log('focus')
+    this.setData({
+      showModal: true
+    })
+  },
+  // 取消
+  cancleInput(e) {
+    this.setData({
+      showModal: false
+    })
+  },
+  // 输入 - 推荐相关热点 
+  bindInput(e) {
+    console.log(e.detail.value)
+  },
+  // 搜索
+  bindConfirm(e) {
+    console.log(e.detail.value)
   }
 })
 
