920aea0ff93ec91910d8a96829bdfa22e26d319e
xhc <1031568754@qq.com>
2019-02-28 00:17:28 +0800

    finish commitlog 样式

--- pages/commitlog/commitlog.js
+++ pages/commitlog/commitlog.js
@@ -14,7 +14,8 @@ Page({
       { text: '前端', type: 'front' }
     ],
     update: '',
-    listData: []
+    listData: [],
+    nodata: false
   },
   onLoad() {
     this.init(this.data.typeList[this.data.selectedIndex].type)
@@ -22,11 +23,12 @@ Page({
   init(type) {
     if (!allData[type]) { // 无数据 请求
       request.fetchCommitLog(type).then(res => {
-        if (res) {
-          let update = res.update
+        if (res && res.data.length) {
+          let update = new Date(res.update - 57600000).toISOString().slice(0, 10)
           let list = res.data.map(item => {
+            let time = new Date(new Date(item.date).getTime() + 28800000).toISOString()
             return { // 2019-01-02 
-              time: new Date(new Date(item.date).getTime() + 28800000).toISOString(),  
+              time: `${time.slice(0, 10)} ${time.slice(11, 16)}`,  
               msg: item.msg
             }
           })
@@ -38,7 +40,15 @@ Page({
             update: update,
             listData: list
           })
+        } else {
+          this.setData({
+            noData: true
+          })
         }
+      }).catch(err => {
+        this.setData({
+          noData: true
+        })
       })
     } else {  // 有数据
       this.setData({
